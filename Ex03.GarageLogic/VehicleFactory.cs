b2ff5d6041ee6f89aa3a2dd3a92e116d40c30263
or keren <orkeren8@gmail.com>
2025-01-08 20:44:47 +0200

    create vehicle factory file

---
+++ Ex03.GarageLogic/VehicleFactory.cs
@@ -0,0 +1,16 @@
+﻿namespace Ex03.GarageLogic
+{
+    public class VehicleFactory
+    {
+
+        public Car GetNewCar(eColor i_Color, int i_NumOfDoors)
+        {
+            return new Car(getNewFuelTank(Conf.CarFuelTankSize, Conf.CarBaseFuelType), i_Color, i_NumOfDoors);
+        }
+
+        private FuelTank getNewFuelTank(float i_FuelCapacity, eFuelType i_EnergyType)
+        {
+            return new FuelTank(i_FuelCapacity, i_EnergyType);
+        }
+    }
+}
