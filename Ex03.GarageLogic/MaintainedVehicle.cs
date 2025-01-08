6cc8eb662c1ed9741b650f905612320361448b3c
or keren <orkeren8@gmail.com>
2025-01-08 19:36:51 +0200

    crate maintained car class

---
+++ Ex03.GarageLogic/MaintainedVehicle.cs
@@ -0,0 +1,28 @@
+﻿namespace Ex03.GarageLogic
+{
+    public enum eMaintenanceStatus
+    {
+        InFix,
+        Fixed,
+        Paid
+    }
+    internal class MaintainedVehicle
+    {
+        string m_OwnerName;
+        string m_PhoneNumber;
+        eMaintenanceStatus m_Status;
+        Vehicle m_Vehicle;
+
+        public MaintainedVehicle(string i_OwnerName,
+                                 string i_PhoneNumber, 
+                                 Vehicle i_Vehicle)
+        {
+            m_OwnerName = i_OwnerName;
+            m_PhoneNumber = i_PhoneNumber;
+            m_Status = eMaintenanceStatus.InFix;
+            m_Vehicle = i_Vehicle;
+        }
+    }
+
+}
+
