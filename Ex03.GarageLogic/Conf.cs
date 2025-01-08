96716b0f4d43e84ebd1a7557f0d0d2f2eed0421f
or keren <orkeren8@gmail.com>
2025-01-08 20:44:10 +0200

    create configure file for all of the base parameters of vehicles

---
+++ Ex03.GarageLogic/Conf.cs
@@ -0,0 +1,12 @@
+﻿namespace Ex03.GarageLogic
+{
+    public class Conf
+    {
+        public const float CarFuelTankSize = 52; //in litters
+        public const eFuelType CarBaseFuelType = eFuelType.Octan95;
+        public const float CarAirPressure = 32;  //psi
+        public const int CarAmountOfWheels = 5;
+        
+        public const float CarMaxChargeTime = (float)2.9; //hours
+    }
+}
