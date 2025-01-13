277cb3ea1e0bce3005a08171a33931e83d117328
or keren <orkeren8@gmail.com>
2025-01-13 22:07:36 +0200

    add roy data

---
+++ UI/Menu.cs
@@ -0,0 +1,16 @@
+﻿namespace UI
+{
+    public class Menu
+    {
+        public enum eMenuSelect
+        {
+            EnterNewVehicleToGarage = 1,
+            ShowAllVehicleLicenseNumber = 2,
+            ChangeVehicleStatus = 3,
+            InflateVehicleTiresToMaximum = 4,
+            RefuelFuelVehicle = 5,
+            ChargeElectricVehicle = 6,
+            GetDetailsOfVehicleByLicenseNumber = 7
+        }
+    }
+}
