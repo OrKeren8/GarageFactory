54ba82aaa8bc42c991d786759d2049307bef49d2
or keren <orkeren8@gmail.com>
2025-01-15 17:20:36 +0200

    get rid from enum number - not scaleable

--- UI/Menu.cs
+++ UI/Menu.cs
@@ -4,13 +4,13 @@
     {
         public enum eMenuSelect
         {
-            EnterNewVehicleToGarage = 1,
-            ShowAllVehicleLicenseNumber = 2,
-            ChangeVehicleStatus = 3,
-            InflateVehicleTiresToMaximum = 4,
-            RefuelFuelVehicle = 5,
-            ChargeElectricVehicle = 6,
-            GetDetailsOfVehicleByLicenseNumber = 7
+            EnterNewVehicleToGarage,
+            ShowAllVehicleLicenseNumber,
+            ChangeVehicleStatus,
+            InflateVehicleTiresToMaximum,
+            RefuelFuelVehicle,
+            ChargeElectricVehicle,
+            GetDetailsOfVehicleByLicenseNumber
         }
     }
 }
