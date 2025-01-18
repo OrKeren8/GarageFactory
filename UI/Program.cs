91b940e180182bf6d2d99ec2e162d4761c4d21d2
or keren <orkeren8@gmail.com>
2025-01-18 11:10:50 +0200

    working change ui project name

---
+++ UI/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Ex03.ConsoleUI
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            UI.UI ui = new UI.UI();
+
+            ui.ApplicationMainLoop();
+        }
+    }
+
+}
